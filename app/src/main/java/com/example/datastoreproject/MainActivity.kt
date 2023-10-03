22b5cb7229c2cdedb89ba5d025a615be6e4a9948
gxx <745570899@qq.com>
2023-10-03 21:22:00 +0800

    补充案例

--- app/src/main/java/com/example/datastoreproject/MainActivity.kt
+++ app/src/main/java/com/example/datastoreproject/MainActivity.kt
@@ -2,20 +2,16 @@ package com.example.datastoreproject
 
 import android.content.Context
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.gxx.datalibrary.DataStoreUtil
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.util.Random
-import java.util.prefs.Preferences
+import kotlinx.coroutines.withContext
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
@@ -47,24 +43,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val testStringClass = String::class.java
-        if (testStringClass.isAssignableFrom(String::class.java)){
-            Log.d(TAG,"你输入的是String类型")
+        if (testStringClass.isAssignableFrom(String::class.java)) {
+            Log.d(TAG, "你输入的是String类型")
         }
 
         //插入值
-        this.findViewById<Button>(R.id.user_info_shared_insert_int_string).setOnClickListener{
-            userInfoSharePre.edit().apply{
-                putInt(KEY_SHARE_AGE,Random().nextInt())
-                putString(KEY_SHARE_NAME,"用户名字-${Random().nextInt()}")
+        this.findViewById<Button>(R.id.user_info_shared_insert_int_string).setOnClickListener {
+            userInfoSharePre.edit().apply {
+                putInt(KEY_SHARE_AGE, Random().nextInt())
+                putString(KEY_SHARE_NAME, "用户名字-${Random().nextInt()}")
                 commit()
             }
         }
 
         //插入值
-        this.findViewById<Button>(R.id.car_info_shared_insert_int_string).setOnClickListener{
-            carInfoSharePre.edit().apply{
-                putString(KEY_SHARE_BRAND,"品牌型号-${Random().nextInt()}")
-                putString(KEY_SHARE_NUMBERPLATE,"车牌号川-${Random().nextInt()}")
+        this.findViewById<Button>(R.id.car_info_shared_insert_int_string).setOnClickListener {
+            carInfoSharePre.edit().apply {
+                putString(KEY_SHARE_BRAND, "品牌型号-${Random().nextInt()}")
+                putString(KEY_SHARE_NUMBERPLATE, "车牌号川-${Random().nextInt()}")
                 commit()
             }
         }
@@ -75,32 +71,46 @@ class MainActivity : AppCompatActivity() {
         }
 
         //迁移完userInfo，往里面插入一条int类型的值
-        this.findViewById<Button>(R.id.user_sharefr_migrate_data_store_insert_int).setOnClickListener {
-            DataStoreUtil.getInstance().put(SHARE_KEY_USER,KEY_SHARE_AGE,Random().nextInt())
-        }
+        this.findViewById<Button>(R.id.user_sharefr_migrate_data_store_insert_int)
+            .setOnClickListener {
+                DataStoreUtil.getInstance().put(SHARE_KEY_USER, KEY_SHARE_AGE, Random().nextInt())
+            }
 
         //迁移完userInfo，往里面插入一条String类型的值
-        this.findViewById<Button>(R.id.user_sharefr_migrate_data_store_insert_string).setOnClickListener {
-            DataStoreUtil.getInstance().put(SHARE_KEY_USER,KEY_SHARE_NAME,"新插入的用户名字-${Random().nextInt()}")
-        }
+        this.findViewById<Button>(R.id.user_sharefr_migrate_data_store_insert_string)
+            .setOnClickListener {
+                DataStoreUtil.getInstance()
+                    .put(SHARE_KEY_USER, KEY_SHARE_NAME, "新插入的用户名字-${Random().nextInt()}")
+            }
 
         //读取参数
         this.findViewById<Button>(R.id.user_read_data_store_value).setOnClickListener {
-           val result = DataStoreUtil.getInstance().getString(SHARE_KEY_USER,KEY_SHARE_NAME,"")
-            Log.d(TAG,"result=${result}")
+            val result = DataStoreUtil.getInstance().getString(SHARE_KEY_USER, KEY_SHARE_NAME, "")
+            Log.d(TAG, "result=${result}")
         }
 
         //往dataStore里面的 share_user_info 插入对象，然后读取
         this.findViewById<Button>(R.id.user_insert_user_info_obj).setOnClickListener {
-            val testModel = TestModel(1,"王五")
-            //插入
-            DataStoreUtil.getInstance().put(SHARE_KEY_USER,KEY_SHARE_OBJ,testModel)
-            //再次读取处理
-           val saveModel = DataStoreUtil.getInstance().getAny<TestModel>(SHARE_KEY_USER,KEY_SHARE_OBJ,TestModel::class.java)
-            Log.d(TAG,"name=${saveModel?.name}")
-            Log.d(TAG,"sex=${saveModel?.sex}")
-        }
+            lifecycleScope.launch {
+                val testModel = TestModel(1, "王五-${Random().nextInt()}")
+                //插入
+                withContext(Dispatchers.IO) {
+                    DataStoreUtil.getInstance().put(SHARE_KEY_USER, KEY_SHARE_OBJ, testModel)
+                }
+                //再次读取处理
+                withContext(Dispatchers.IO) {
+                    val saveModel = DataStoreUtil.getInstance()
+                        .getAny<TestModel>(SHARE_KEY_USER, KEY_SHARE_OBJ, TestModel::class.java)
+                    Log.d(TAG, "name=${saveModel?.name}")
+                    Log.d(TAG, "sex=${saveModel?.sex}")
+                }
 
+                Log.d(TAG, "完成")
+
+
+            }
+
+        }
     }
 
 
